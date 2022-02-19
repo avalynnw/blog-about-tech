48982b802ae38069d7573ecfe354d60dd2cbc401
Avalynn Judge <avalynnjudge@gmail.com>
2022-02-19 08:17:18 -0800

    Adding posts

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { BlogPost, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newBlogPost = await BlogPost.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newBlogPost);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,14 +17,14 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const blogPostData = await Project.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
+    if (!blogPostData) {
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
@@ -34,5 +34,44 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+
+
+
+
+
+
+router.get('/', async (req, res) => {
+  try {
+    // get all blogPosts and JOIN with user data
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // serialize data so the template can read it
+    const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+
+    // pass serialized data and session flag into handlebars html template
+    res.render('home', { 
+      blogPosts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
+
 
 module.exports = router;
